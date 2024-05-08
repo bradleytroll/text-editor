915b165bd6d7b40b2ede84be62bff0bced591ee8
Bradley Troll <bradleytroll@gmail.com>
2024-05-08 16:39:04 -0400

    Updates src-sw.js file

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -28,8 +28,8 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
 registerRoute(
-  ({ request }) => ['image', 'script', 'style'].includes(request.destination),
-  new CacheFirst({
+  ({ request}) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
     cacheName: 'assets',
     plugins: [
       new CacheableResponsePlugin({
@@ -40,5 +40,4 @@ registerRoute(
       }),
     ],
   })
-
 );
