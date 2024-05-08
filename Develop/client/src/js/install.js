fa1f696270735c52ee1685bf9bfe94a1b23cf361
Bradley Troll <bradleytroll@gmail.com>
2024-05-08 16:35:13 -0400

    Rewrites install.js code

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -2,24 +2,20 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {
-    window.deferredPrompt = event;
-    butInstall.classList.toggle('hidden', false);
 
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    butInstall.style.visibility = 'visible';   
+    butInstall.textContent = 'Install';
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-    const deferredPrompt = window.deferredPrompt;
-    if (!promptEvent) {
-        return;
-    }
-    promptEvent.prompt();
-    window.deferredPrompt = null;
-    butInstall.classList.toggle('hidden', true);
+  butInstall.setAttribute('disabled', true);
+  butInstall.textContent = 'Installed';
 });
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-    window.deferredPrompt = null;
+    console.log('App was installed');
 });
