5f5c3efeca0169e4a3495aaf6ef3a15d4071ccd8
Bradley Troll <bradleytroll@gmail.com>
2024-05-08 16:12:49 -0400

    Completes src-sw.js file.

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -27,4 +27,18 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  ({ request }) => ['image', 'script', 'style'].includes(request.destination),
+  new CacheFirst({
+    cacheName: 'assets',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      }),
+    ],
+  })
+
+);
